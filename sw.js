9ca147ca3d1f05abf7b2c2727a6b4f9b3026c2c3
joan <joannaarivenda@gmail.com>
2025-11-29 13:34:37 +0700

    Fix: Merubah JavaScript yang ada dilaman file sw.js agar menyediakan halam fallback

--- sw.js
+++ sw.js
@@ -1,32 +1,47 @@
-const CACHE_NAME = 'pwa-adr-cache-v1';
-const urlsToCache = [
-  '/ADR-Teknik/',
-  '/ADR-Teknik/index.html',
-  '/ADR-Teknik/manifest.json',
-  '/ADR-Teknik/icons/icon-192x192.png',
-  '/ADR-Teknik/icons/icon-512x512.png',
-  '/ADR-Teknik/assets/css/style.css',
-  // Tambahkan path file lain yang perlu di-cache di sini
-];
+// This is the "Offline page" service worker
 
-self.addEventListener('install', function(event) {
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "Index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-        return fetch(event.request);
-      })
-  );
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
